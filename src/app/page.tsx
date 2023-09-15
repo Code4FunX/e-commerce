573104d73101a351aa433bfbf40ff0f5b81ecb73
caionobrex <marialoucos@hotmail.com.br>
2023-09-15 18:41:55 -0300

    chore: move Header and Footer to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Footer } from '@/components/Footer'
-import { Header } from '../components/Header'
-
 export default function Home() {
   return (
     <main>
-      <Header />
       <div className="min-h-screen"></div>
-      <Footer />
     </main>
   )
 }
