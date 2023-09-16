dfbac5e62283a1faf317abf49824c0cdfc0a4a01
mateus <m4teusmatos@gmail.com>
2023-09-16 15:21:12 -0300

    feat: adding SubHeader

---
+++ src/components/Header/components/SubHeader/index.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { BiMenu } from 'react-icons/bi'
+import { GoChevronDown } from 'react-icons/go'
+import AleFlag from '@/assets/contry-flags/ale-flag.svg'
+
+export const SubHeader = () => {
+  return (
+    <div className="flex w-full justify-center border-b">
+      <div className="custom-container flex w-full items-center justify-between py-3">
+        <div className="flex gap-x-6">
+          <Link href="/">
+            <div className="flex items-center justify-center gap-x-1">
+              <BiMenu className="text-xl text-black" />
+              <span>All category</span>
+            </div>
+          </Link>
+          <Link href="/">
+            <span>Hot offers</span>
+          </Link>
+          <Link href="/">
+            <span>Gift boxes</span>
+          </Link>
+          <Link href="/">
+            <span>Projects</span>
+          </Link>
+          <Link href="/">
+            <span>Menu item</span>
+          </Link>
+          <Link href="/">
+            <div className="flex items-center justify-center gap-x-1">
+              <span>Help</span>
+              <GoChevronDown className="text-xl text-gray-500" />
+            </div>
+          </Link>
+        </div>
+        <div className="flex gap-x-6">
+          <Link href="/">
+            <div className="flex items-center justify-center gap-x-1">
+              <span>English, USD</span>
+              <GoChevronDown className="text-xl text-gray-500" />
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="flex items-center justify-center gap-x-2">
+              <div className="flex items-center justify-center gap-x-1.5">
+                <span>Ship to</span>
+                <Image src={AleFlag} alt="logo" width={22} />
+              </div>
+              <GoChevronDown className="text-xl text-gray-500" />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,47 +4,59 @@ import { MdMessage } from 'react-icons/md'
 import { GoChevronDown } from 'react-icons/go'
 import LogoWithName from '@/assets/logo-with-name.svg'
 import Link from 'next/link'
+import { SubHeader } from './components/SubHeader'
 
 export const Header = () => {
   return (
-    <header className="flex w-full justify-center bg-white py-4">
-      <div className="custom-container flex w-full items-center justify-between">
-        <Link href="/">
-          <Image src={LogoWithName} alt="logo" width={150} height={56} />
-        </Link>
-        <div className="hidden h-10 flex-1 px-24 md:flex">
-          <input
-            className="w-full rounded-s-md border-2 border-blue-600 px-3"
-            type="text"
-            placeholder="Search"
-          />
-          <div className="flex h-auto items-center justify-center gap-x-3 border-y-2 border-e-2 border-blue-600 px-2">
-            <span className="text-base">Categorias</span>
-            <GoChevronDown className="text-xl text-gray-500" />
+    <>
+      <header className="flex w-full justify-center border-b bg-white py-4 ">
+        <div className="custom-container flex w-full items-center justify-between">
+          <Link href="/">
+            <Image src={LogoWithName} alt="logo" width={150} height={56} />
+          </Link>
+          <div className="hidden h-10 flex-1 px-24 md:flex">
+            <input
+              className="w-full rounded-s-md border-2 border-blue-600 px-3"
+              type="text"
+              placeholder="Search"
+            />
+            <div className="flex h-auto items-center justify-center gap-x-3 border-y-2 border-e-2 border-blue-600 px-2">
+              <span className="text-base">Categorias</span>
+              <GoChevronDown className="text-xl text-gray-500" />
+            </div>
+            <button className="rounded-e-md bg-blue-600 px-4 text-white">
+              Pesquisar
+            </button>
           </div>
-          <button className="rounded-e-md bg-blue-600 px-4 text-white">
-            Pesquisar
-          </button>
+          <div className="hidden items-center justify-center gap-x-5 md:flex">
+            <Link href="/">
+              <div className="flex flex-col items-center gap-y-0.5">
+                <FaUser className="text-xl text-gray-500" />
+                <span className="text-gray-500">Perfil</span>
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="flex flex-col items-center gap-y-0.5">
+                <MdMessage className="text-xl text-gray-500" />
+                <span className="text-gray-500">Mensagem</span>
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="flex flex-col items-center gap-y-0.5">
+                <FaHeart className="text-xl text-gray-500" />
+                <span className="text-gray-500">Pedido</span>
+              </div>
+            </Link>
+            <Link href="/">
+              <div className="flex flex-col items-center gap-y-0.5">
+                <FaShoppingCart className="text-xl text-gray-500" />
+                <span className="text-gray-500">Carrinho</span>
+              </div>
+            </Link>
+          </div>
         </div>
-        <div className="hidden items-center justify-center gap-x-5 md:flex">
-          <div className="flex flex-col items-center gap-y-0.5">
-            <FaUser className="text-xl text-gray-500" />
-            <span className="text-gray-500">Perfil</span>
-          </div>
-          <div className="flex flex-col items-center gap-y-0.5">
-            <MdMessage className="text-xl text-gray-500" />
-            <span className="text-gray-500">Mensagem</span>
-          </div>
-          <div className="flex flex-col items-center gap-y-0.5">
-            <FaHeart className="text-xl text-gray-500" />
-            <span className="text-gray-500">Pedido</span>
-          </div>
-          <div className="flex flex-col items-center gap-y-0.5">
-            <FaShoppingCart className="text-xl text-gray-500" />
-            <span className="text-gray-500">Carrinho</span>
-          </div>
-        </div>
-      </div>
-    </header>
+      </header>
+      <SubHeader></SubHeader>
+    </>
   )
 }
