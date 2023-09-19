1391b41fe47b25f3fa1437509e839c0a04a38624
caionobrex <marialoucos@hotmail.com.br>
2023-09-18 21:56:05 -0300

    feat: add min-h-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Banner } from './components/Banner'
 
 export default function Home() {
   return (
-    <main className="custom-container flex w-full">
+    <main className="custom-container min-h-screen w-full">
       <Banner />
     </main>
   )
